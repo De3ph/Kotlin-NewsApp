799ab99ca39ae9b81137095e72b0534e3baf3705
De3ph <05190000108@ogrenci.ege.edu.tr>
2023-11-21 17:21:49 +0300

    feat: onboarding screen

--- app/src/main/java/com/loc/newsapp/MainActivity.kt
+++ app/src/main/java/com/loc/newsapp/MainActivity.kt
@@ -3,7 +3,12 @@ package com.loc.newsapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.loc.newsapp.presentation.onboarding.OnBoardingScreen
 import com.loc.newsapp.ui.theme.NewsAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,7 +17,9 @@ class MainActivity : ComponentActivity() {
         installSplashScreen()
         setContent {
             NewsAppTheme {
-
+                Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
+                    OnBoardingScreen()
+                }
             }
         }
     }
---
+++ app/src/main/java/com/loc/newsapp/presentation/onboarding/Page.kt
@@ -0,0 +1,29 @@
+package com.loc.newsapp.presentation.onboarding
+
+import androidx.annotation.DrawableRes
+import com.loc.newsapp.R
+
+data class Page(
+    val title: String,
+    val description: String,
+    @DrawableRes val image: Int,
+)
+
+
+val pages = listOf<Page>(
+    Page(
+        "Lorem ipsum is simply dummy",
+        "Lorem ipsum is simply dummy text of the printing and typesetting inddustry",
+        R.drawable.onboarding1
+    ),
+    Page(
+        "Lorem ipsum is simply dummy",
+        "Lorem ipsum is simply dummy text of the printing and typesetting inddustry",
+        R.drawable.onboarding2
+    ),
+    Page(
+        "Lorem ipsum is simply dummy",
+        "Lorem ipsum is simply dummy text of the printing and typesetting inddustry",
+        R.drawable.onboarding3
+    ),
+)
